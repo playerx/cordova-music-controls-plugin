470f0c0451bcda7ab0057ca663a896caae9f556b
Ezeki Zibzibadze <playerx@users.noreply.github.com>
2017-09-18 14:12:30 +0400

    Update MusicControlsInfo.m

--- src/ios/MusicControlsInfo.m
+++ src/ios/MusicControlsInfo.m
@@ -23,6 +23,16 @@
        [self setIsPlaying: [[dictionary objectForKey:@"isPlaying"] boolValue]];
        [self setHasPrev: [[dictionary objectForKey:@"hasPrev"] boolValue]];
        [self setHasNext: [[dictionary objectForKey:@"hasNext"] boolValue]];
+      
+       [self setHasLike: [[dictionary objectForKey:@"hasLike"] boolValue]];
+       [self setHasDislike: [[dictionary objectForKey:@"hasDislike"] boolValue]];
+       [self setHasBookmark: [[dictionary objectForKey:@"hasBookmark"] boolValue]];
+      
+       [self setLikeTitle: [dictionary objectForKey:@"likeTitle"]];
+       [self setDislikeTitle: [dictionary objectForKey:@"dislikeTitle"]];
+       [self setBookmarkTitle: [dictionary objectForKey:@"bookmarkTitle"]];
+       [self setPreviousTrackTitle: [dictionary objectForKey:@"previousTrackTitle"]];
+      
        [self setHasSkipForward: [[dictionary objectForKey:@"hasSkipForward"] boolValue]];
        [self setHasSkipBackward: [[dictionary objectForKey:@"hasSkipBackward"] boolValue]];
        [self setSkipForwardInterval: [[dictionary objectForKey:@"skipForwardInterval"] integerValue]];
